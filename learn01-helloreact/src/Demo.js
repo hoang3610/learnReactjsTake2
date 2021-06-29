cf7e9ef9e7c6e7ef7f45d57100fb508354e3a81a
HoangPV <hoang3610@gmail.com>
2021-06-29 15:17:16 +0700

    learnreactjs01-helloworld

---
+++ learn01-helloreact/src/Demo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Demo extends Component {
+	render () {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Chao mung den voi trang web cua Hoang</p>
+      </div>
+    );
+  }
+}
+
+
+export default Demo;
+
